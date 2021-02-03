d2c0904c3ee99b77c3d8c74ee4fe299955793d02
Goose <60141816+elegoose@users.noreply.github.com>
2021-02-03 19:41:30 -0300

    stop and playerlist commands added

--- ServerBot.py
+++ ServerBot.py
@@ -23,10 +23,12 @@ async def on_command_error(ctx, error):
 
 @client.command(brief="abre el servidor")
 async def abrir(ctx, servername):
-    for server in activeServers:
-        if servername in sv.serverNames and server.isRunning:
-            raise sv.ServerAlreadyRunning
     servername = servername.lower()
+    if servername not in sv.serverNames:
+        raise sv.ServerNotFound
+    for server in activeServers:
+        if servername in server.nameArray:
+            raise sv.ServerAlreadyRunning
     server = sv.Server()
     server.name = servername
     await server.run(ctx, serverMacAdress)
@@ -45,13 +47,97 @@ async def abrir_error(ctx, error):
             await ctx.send('Aún no implemento este servidor al bot :sob:')
         elif isinstance(error, sv.CouldntAccessRemoteServer):
             await ctx.send('Por alguna razón, no se puede acceder al PC del server.')
+        elif isinstance(error, sv.UnexpectedRunError):
+            await ctx.send('Error inesperado en método run')
+            raise error
         else:
             await ctx.send(f'Error de invocación inesperado: {error}')
             raise error
     elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f'Ningún servidor seleccionado para abrir. Estos son los disponibles:')
+        await ctx.send(f'Ningún servidor seleccionado para abrir.')
     else:
         await ctx.send(f'Error inesperado: {error}')
+        raise error
+
+
+@client.command(brief="cierra el servidor")
+async def cerrar(ctx, servername):
+    servername = servername.lower()
+    if servername not in sv.serverNames:
+        raise sv.ServerNotFound
+    isRunning = False
+    for server in activeServers:
+        if servername in server.nameArray:
+            isRunning = True
+            await server.stop(ctx, activeServers)
+            activeServers.remove(server)
+    if not isRunning:
+        raise sv.ServerIsNotRunning
+
+
+@cerrar.error
+async def cerrar_error(ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+        error = error.original
+        if isinstance(error, sv.ServerIsNotRunning):
+            await ctx.send('El servidor no está abierto, por lo tanto no se puede cerrar.')
+        elif isinstance(error, sv.ServerNotFound):
+            await ctx.send('No he encontrado ningún servidor para cerrar con ese nombre.')
+        elif isinstance(error, sv.CantClosePopulatedServer):
+            await ctx.send('No se puede cerrar el servidor porque hay jugadores dentro.')
+        else:
+            await ctx.send(f'Error de invocación inesperado: {error}\nAvísale al creador para que lo arregle.')
+            raise error
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'Ningún servidor seleccionado para cerrar.')
+    else:
+        await ctx.send(f'Error inesperado: {error}\nAvísale al creador para que lo arregle.')
+        raise error
+
+
+@client.command(brief='muestra la lista de jugadores en el servidor seleccionado',
+                aliases=['playerlist', 'lista', 'jugando'])
+async def jugadores(ctx, servername):
+    servername = servername.lower()
+    if servername not in sv.serverNames:
+        raise sv.ServerNotFound
+
+    isRunning = False
+    playerlist = None
+    gameName = None
+    for server in activeServers:
+        if servername in server.nameArray:
+            playerlist = await server.playerlist(activeServers)
+            gameName = server.GameName
+            isRunning = True
+
+    if not isRunning:
+        raise sv.ServerIsNotRunning
+
+    if not playerlist:
+        await ctx.send(f'No hay nadie jugando en el servidor de {gameName}')
+        return
+    playerlistString = '\n'
+    for player in playerlist:
+        playerlistString += player + '\n'
+    await ctx.send(f'Jugadores en servidor de {gameName}:{playerlistString}')
+
+
+@jugadores.error
+async def jugadores_error(ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+        error = error.original
+        if isinstance(error, sv.ServerIsNotRunning):
+            await ctx.send('El servidor no está abierto.')
+        elif isinstance(error, sv.ServerNotFound):
+            await ctx.send('No he encontrado ningún servidor con ese nombre.')
+        else:
+            await ctx.send(f'Error de invocación inesperado: {error}\nAvísale al creador para que lo arregle.')
+            raise error
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'Ningún servidor seleccionado para ver quién está jugando en ellos.')
+    else:
+        await ctx.send(f'Error inesperado: {error}\nAvísale al creador para que lo arregle.')
         raise error
 
 
--- servers.py
+++ servers.py
@@ -1,10 +1,17 @@
 import subprocess
 from wakeonlan import send_magic_packet
+from mcstatus import MinecraftServer
+from mcrcon import MCRcon
+import socket
 
 minecraftNameArray = ['mc', 'minecraft', 'mine']
 conanExilesNameArray = ['conan', 'conanexiles']
 killingFloorNameArray = ['kf2', 'kf', 'killingfloor2', 'killingfloor']
 serverNames = [*minecraftNameArray, *conanExilesNameArray, *killingFloorNameArray]
+with open('credentials.txt') as f:
+    credentialsArray = f.read().splitlines()
+localServerIp = credentialsArray[1]
+rconPass = credentialsArray[3]
 
 
 class ServerNotFound(Exception):
@@ -23,22 +30,39 @@ class CouldntAccessRemoteServer(Exception):
     pass
 
 
+class UnexpectedRunError(Exception):
+    pass
+
+
+class ServerIsNotRunning(Exception):
+    pass
+
+
+class CantClosePopulatedServer(Exception):
+    pass
+
+
 class Server:
     def __init__(self):
         self.name = ''
-        self.isRunning = False
+        # self.isRunning = False
         self.GameName = ''
+        self.nameArray = []
+        self.minecraftserver = None
 
-    async def run(self, ctx, macAdress):
+    async def run(self, ctx, macadress):
         if self.name in minecraftNameArray:
-            send_magic_packet(macAdress)
+            send_magic_packet(macadress)
             self.GameName = 'Minecraft'
+            self.nameArray = minecraftNameArray
             await ctx.send(f'Abriendo server de {self.GameName}...')
             p = subprocess.run("RunMinecraftServer.bat", stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
             err = p.stderr
             if err[2:35] == 'remote_abrir_mcserver.bat started':
-                self.isRunning = True
-                await ctx.send(f'Server abierto.')
+                # self.isRunning = True
+                await ctx.send('Server abierto.')
+                self.minecraftserver = MinecraftServer(localServerIp)
+
             else:
                 raise CouldntAccessRemoteServer
 
@@ -53,4 +77,40 @@ class Server:
             # self.GameName = 'Conan Exiles'
             raise ServerNotYetImplemented
         else:
-            raise ServerNotFound
+            raise UnexpectedRunError
+
+    async def stop(self, ctx, activeServers):  # noqa
+        if self.GameName == 'Minecraft':
+            if await self.isNotEmpty(activeServers):
+                raise CantClosePopulatedServer
+            await ctx.send(f'Cerrando server de {self.GameName}...')
+            with MCRcon(localServerIp, rconPass) as mcr:
+                mcr.command("stop")
+            await ctx.send('Server cerrado.')
+
+    async def playerlist(self, activeServers):  # noqa
+        if self.GameName == 'Minecraft':
+            try:
+                query = self.minecraftserver.query()
+            except socket.timeout:
+                await self.removeFromActiveServers(activeServers)
+                raise ServerIsNotRunning
+            playerlist = query.players.names
+            return playerlist
+
+    async def isNotEmpty(self, activeServers):  # noqa
+        if self.GameName == 'Minecraft':
+            try:
+                status = self.minecraftserver.status()
+            except socket.timeout:
+                await self.removeFromActiveServers(activeServers)
+                raise ServerIsNotRunning
+            if status.players.online == 0:
+                return False
+            else:
+                return True
+
+    async def removeFromActiveServers(self, activeServers):  # noqa
+        for server in activeServers:
+            if server.GameName == self.GameName:
+                activeServers.remove(server)
